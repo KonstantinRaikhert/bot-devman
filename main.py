da4f2b46f5523ce85e7365f932c28cce09142dbc
Konstantin Raikhert <raikhert13@gmail.com>
2022-03-24 01:21:06 +0300

    complete send message to telegram

--- main.py
+++ main.py
@@ -3,30 +3,52 @@ import os
 import requests
 from dotenv import load_dotenv
 
+from bot import send_message
+
 load_dotenv()
 
-url = "https://dvmn.org/api/long_polling/"
-# url ='https://dvmn.org/api/user_reviews/'
-token = os.environ.get("DEVMAN_TOKEN")
+ENDPOINT = "https://dvmn.org/api/long_polling/"
+TOKEN = os.environ.get("DEVMAN_TOKEN")
+
+
+def homework_status(status):
+    """Возвращает статус проверки работы в словах."""
+    if status:
+        return "Работа нуждается в доработке."
+    return "Работа сдана!"
+
 
 timestamp = 0
 
 
-def get(url):
-    """Что делает этот метод."""
+def get_status_homework(endpoint, student_name, chat_id):
+    """Отправляем статус проверки работы."""
+    global timestamp
     try:
-        global timestamp
-        print(timestamp)
         payload = {"timestamp_to_request": timestamp}
-        response = requests.get(url, headers={"Authorization": token}, params=payload)
-        timestamp = response.json()["timestamp_to_request"]
-        print(response.json())
+        response = requests.get(endpoint, headers={"Authorization": TOKEN}, params=payload)
+        data = response.json()
+        timestamp = data.get("timestamp_to_request")
+
+        if data["status"] == "found":
+            lesson_title = data["new_attempts"][0]["lesson_title"]
+            is_negative = homework_status(data["new_attempts"][0]["is_negative"])
+            lesson_url = data["new_attempts"][0]["lesson_url"]
+            name = student_name
+            text = f"{name}, урок '{lesson_title}' проверен. {is_negative} Ссылка на урок - {lesson_url}"
+            send_message(chat_id=chat_id, text=text)
     except requests.exceptions.ReadTimeout as time_out_error:
         print(time_out_error)
     except requests.exceptions.ConnectionError:
-        print("Интеренет вырубился")
+        print("Интернет вырубился")
 
 
 if __name__ == "__main__":
+    student_name = input("Введите Ваше имя: \n")
+    chat_id = input("Введите Ваш chat_id: \n")
     while True:
-        get(url)
+        get_status_homework(
+            endpoint=ENDPOINT,
+            student_name=student_name,
+            chat_id=chat_id,
+        )
