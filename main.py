29bdeba378abf417f5d20ece85c6116d57321162
Konstantin Raikhert <raikhert13@gmail.com>
2022-03-23 23:26:33 +0300

    initial

---
+++ main.py
@@ -0,0 +1,31 @@
+import requests
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+url = 'https://dvmn.org/api/long_polling/'
+# url ='https://dvmn.org/api/user_reviews/'
+token = os.environ.get('DEVMAN_TOKEN')
+
+timestamp = 0
+
+
+def get(url):
+    try:
+        global timestamp
+        print(timestamp)
+        payload = {'timestamp_to_request': timestamp}
+        response = requests.get(url, headers={'Authorization': token}, params=payload)
+        timestamp = response.json()['timestamp_to_request']
+        print(response.json())
+    except requests.exceptions.ReadTimeout as time_out_error:
+        print(time_out_error)
+    except requests.exceptions.ConnectionError:
+        print("Интеренет вырубился")
+
+
+if __name__ == '__main__':
+    while True:
+        get(url)
