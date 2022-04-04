b9500ba06ca853475cc88ead099bc971f3c1088f
Konstantin Raikhert <69113745+KonstantinRaikhert@users.noreply.github.com>
2022-04-05 01:25:01 +0300

    Develop (#2)
    * initial
    
    * delete pycharm settings
    
    * delete pycharm settings
    
    * pre-commit install
    
    * complete send message to telegram
    
    * some refactor
    
    * refactor requests
    
    * after review 2
    
    * configuration to start on vps
    
    * deploy to heroku
    
    * add procfile
    
    * console logging
    
    * send logs to Telegram to admin user
    
    * write README

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM python:3.9-slim-bullseye
 WORKDIR /code
 COPY .  .
-COPY requirements/ requirements/
 RUN python -m pip install --upgrade pip
-RUN pip install -r requirements/dev.txt
+RUN pip install -r requirements.txt
 CMD python3 main.py
--- logging_config.py
+++ logging_config.py
@@ -3,6 +3,7 @@ LOGGING_CONFIG = {
     "disable_existing_loggers": True,
     "formatters": {
         "standard": {"format": "%(asctime)s [%(levelname)s] %(name)s: %(message)s"},
+        "tg_formatter": {"format": "[%(levelname)s] %(name)s: %(message)s"},
     },
     "handlers": {
         "file": {
@@ -10,9 +11,18 @@ LOGGING_CONFIG = {
             "formatter": "standard",
             "filename": "bot_chat.log",
             "maxBytes": 1024,
-        }
+        },
+        "telegram": {
+            "class": "logging_telegram_handler.TelegramLogsHandler",
+            "formatter": "tg_formatter",
+        },
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "standard",
+        },
     },
     "loggers": {
-        "__main__": {"handlers": ["file"], "level": "INFO", "propagate": False},
+        "__main__": {"handlers": ["file", "console", "telegram"], "level": "INFO", "propagate": False},
+        }
     },
 }
