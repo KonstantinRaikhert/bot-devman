9801932226137c938c647dd1d72238ce5cabb850
Konstantin Raikhert <raikhert13@gmail.com>
2022-04-14 01:53:21 +0300

    environments in main

--- logging_telegram_handler.py
+++ logging_telegram_handler.py
@@ -1,19 +1,12 @@
 import logging
-import os
-
-import telegram
 
 
 class TelegramLogsHandler(logging.StreamHandler):
-    def __init__(self):
+    def __init__(self, telegram_chat_id, telegram_bot):
         super().__init__()
-        self.chat_id = os.environ.get("CHAT_ID")
-        self.telegram_token = os.environ.get("TELEGRAM_TOKEN")
-
-    @property
-    def tg_bot(self):
-        return telegram.Bot(token=self.telegram_token)
+        self.chat_id = telegram_chat_id
+        self.telegram_bot = telegram_bot
 
     def emit(self, record):
         log_entry = self.format(record)
-        self.tg_bot.sendMessage(chat_id=self.chat_id, text=log_entry)
+        self.telegram_bot.sendMessage(chat_id=self.chat_id, text=log_entry)
